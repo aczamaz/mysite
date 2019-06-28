931e6aff9d9a21adc707b3805c078c71ef6b2d7f
Aczamaz Valiev <aczamaz@gmail.com>
2019-06-28 12:31:44 +0300

    первое сохранение

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Шахматы</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<table>
+<?php
+for($i=0; $i<8; $i++){
+echo "<tr>";
+for($j=0; $j<8; $j++){
+if(($i+$j)%2!=0){
+echo "<td class='black'>".$i.$j."</td>";
+}else{
+echo "<td class='white'>".$i.$j."</td>";
+};
+};
+echo "</tr>";
+};
+?>
+</table>
+</body>
+</html>
